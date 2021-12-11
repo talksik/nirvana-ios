315fe6210b4be8a061d9f45332c2f180a6303c98
talksik <patel.arjun50@gmail.com>
2021-12-11 11:58:46 -0800

    Initial Commit

---
+++ nirvana-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nirvana-ios/nirvana_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  nirvana_iosApp.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct nirvana_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
