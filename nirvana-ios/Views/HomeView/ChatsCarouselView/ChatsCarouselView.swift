9bf4798893c8ae407dc0657f094f65847c5373aa
talksik <patel.arjun50@gmail.com>
2021-12-11 19:26:49 -0800

    Update ChatsCarouselView.swift

--- nirvana-ios/Views/HomeView/ChatsCarouselView/ChatsCarouselView.swift
+++ nirvana-ios/Views/HomeView/ChatsCarouselView/ChatsCarouselView.swift
@@ -24,7 +24,7 @@ struct ChatsCarouselView: View {
                         .blur(radius: abs(minX) / 40)
 
                     Text("\(minX)")
-                }gi
+                }
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .always))
