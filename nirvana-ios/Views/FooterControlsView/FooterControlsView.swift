48a720640847e76eafecdf1993fb873fe8370ca5
talksik <patel.arjun50@gmail.com>
2021-12-12 14:04:04 -0800

    getting legit button feel to the record button

--- nirvana-ios/Views/FooterControlsView/FooterControlsView.swift
+++ nirvana-ios/Views/FooterControlsView/FooterControlsView.swift
@@ -15,42 +15,20 @@ struct FooterControlsView: View {
         HStack(alignment: .center) {
             Spacer()
             
-            Image(systemName: viewModel.isRecording ? "waveform" : "mic.fill")
-                .foregroundColor(.white)
-                .padding()
-                .background(NirvanaColor.teal)
-                .clipShape(Circle())
-                .shadow(radius: 10)
-//                .scaleEffect(self.scaleValue)
-                .onLongPressGesture {
-                    
-                }
-                .animation(.linear(duration:2), value: viewModel.isRecording)
-                .onTapGesture {
-                    viewModel.onStartRecording()
-                    print("tap gesture active")
-                }
-//                .onTapGesture(
-//                    touchBegan: {
-//                    withAnimation {
-//                        self.scaleValue = 1.2
-//                    }
-//
-//                    viewModel.onStartRecording()
-//
-//                    print("long press active")
-//                }, touchEnd: {_ in
-//                    withAnimation {
-//                        self.scaleValue = 1.0
-//                    }
-//
-//                    print("long press not active anymore")
-//                }
-                
+            Button(action: {
+                print("pressed button")
+            }) {
+                Image(systemName: viewModel.isRecording ? "waveform" : "mic.fill")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(NirvanaColor.teal)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
             }
-            .frame(maxWidth: .infinity)
-            .padding()
+            
         }
+        .padding()
+    }
 }
 
 struct FooterControlsView_Previews: PreviewProvider {
