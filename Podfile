705203d7bd4d8f5e76999a30c703ee90bdb0c14c
talksik <patel.arjun50@gmail.com>
2021-12-13 12:34:20 -0800

    adding pods method of package managing

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'nirvana-ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for nirvana-ios
+  pod 'FirebaseUI'
+  pod 'FirebaseUI/Phone'
+  pod 'FirebaseUI/Auth'
+  pod 'FirebaseUI/Facebook'
+  pod 'FirebaseUI/OAuth'
+  pod 'FirebaseUI/Google'
+
+end
--- nirvana-ios/Views/Templates/OnboardingTemplateView.swift
+++ nirvana-ios/Views/Templates/OnboardingTemplateView.swift
@@ -43,19 +43,36 @@ struct OnboardingTemplateView: View {
             HeaderView()
             
             VStack {
+                self.onboardingHeaderText
+                
                 self.onboardingImageView
                 
                 self.onboardingActionTextView
                 
+                Spacer()
+                
                 self.bottomActionArea
             }
-            .padding(screenWidth * 0.1)
+            .padding(.horizontal, screenWidth * 0.1)
 
             Spacer()
         } //outermost vstack
         .accentColor(NirvanaColor.teal)
         .background(NirvanaColor.bgLightGrey)
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private var onboardingHeaderText: some View {
+        ZStack {
+            if (self.headerText != nil) {
+                Text(self.headerText!)
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(NirvanaColor.teal)
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .padding()
     }
     
     private var onboardingImageView: some View {
@@ -96,7 +113,7 @@ struct OnboardingTemplateView: View {
 
 struct OnboardingTemplateView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingTemplateView(imgName: "undraw_friendship_mni7", mainLeadingActText: "Your", mainHighlightedActText: "minimalist", mainTrailingActText: "social media.", subActText: "Tired of the rat race on the test of th4e best oadf the best?", bottomActArea: AnyView(
+        OnboardingTemplateView(hdrText: "let's get started", imgName: "undraw_friendship_mni7", mainLeadingActText: "Your", mainHighlightedActText: "minimalist", mainTrailingActText: "social media.", subActText: "Tired of the rat race on the test of th4e best oadf the best?", bottomActArea: AnyView(
                 VStack(alignment: .center) {
                     NavigationLink(destination: HomeView()) {
                         Text("Start Your Detox")
