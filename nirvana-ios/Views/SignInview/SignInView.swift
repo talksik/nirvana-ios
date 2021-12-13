84e4e1d66a63a36a61dc4dd6e6ad491a3d762dce
talksik <patel.arjun50@gmail.com>
2021-12-13 12:58:06 -0800

    clean out pods and have new signin page

---
+++ nirvana-ios/Views/SignInview/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/13/21.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("asdf")
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
