8c207082ed9f94345d34d9cf42c9c5c84bae8165
talksik <patel.arjun50@gmail.com>
2021-12-13 02:46:29 -0800

    adding in basics of firebase

---
+++ nirvana-ios/Views/Templates/OnboardingTemplateView.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingTemplateView.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/13/21.
+//
+
+import SwiftUI
+
+struct OnboardingTemplateView: View {
+    
+    
+    init() {
+        
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+        }
+        
+    }
+}
+
+struct OnboardingTemplateView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingTemplateView()
+    }
+}
