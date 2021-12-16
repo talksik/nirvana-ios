6a020d6c32034e5736e6b9b989f5fdea789cfd0f
talksik <patel.arjun50@gmial.com>
2021-12-16 12:18:59 -0800

    cleaning up home page background

--- nirvana-ios/v2/InnerCircle/InnerCircleView.swift
+++ nirvana-ios/v2/InnerCircle/InnerCircleView.swift
@@ -15,111 +15,83 @@ struct InnerCircleView: View {
     
     var body: some View {
         ZStack {
-            AngularGradient(
-                gradient: Gradient(
-                    colors: [NirvanaColor.solidBlue, NirvanaColor.dimTeal, Color.orange, NirvanaColor.solidBlue]),
-                center: .center,
-                angle: .degrees(120))
-             
-            LinearGradient(gradient: Gradient(
-                colors: [NirvanaColor.white.opacity(0), NirvanaColor.white.opacity(1.0)]), startPoint: .bottom, endPoint: .top)
-            
-            // wave #1
-            Path {path in
-                path.move(
-                    to: CGPoint(
-                        x: 0,
-                        y: baseLineY
-                    )
-                )
-
-                path.addCurve(
-                    to: CGPoint(x: universalSize.width, y: baseLineY),
-                    control1: CGPoint(x: universalSize.width * 0.4, y: baseLineY + 150),
-                    control2: CGPoint(x: universalSize.width * 0.65, y: baseLineY - 100))
-                
-                path.addCurve(
-                    to: CGPoint(x: 2*universalSize.width, y: baseLineY),
-                    control1: CGPoint(x: universalSize.width * 1.4, y: baseLineY + 150),
-                    control2: CGPoint(x: universalSize.width * 1.65, y: baseLineY - 100))
-                
-                path.addLine(to: CGPoint(x: 2*universalSize.width, y: universalSize.height))
-                path.addLine(to: CGPoint(x: 0, y: universalSize.height))
-
+            VStack {
+                HStack {
+                    Image("undraw_handcrafts_leaf")
+                        .resizable()
+                        .frame(width: 20.0, height: 32.132)
+                    
+                    Text("nirvana")
+                        .font(Font.custom("Satisfy-Regular", size: 35))
+                        .foregroundColor(NirvanaColor.teal)
+                        .multilineTextAlignment(.center)
+                }
             }
-            .foregroundColor(NirvanaColor.dimTeal.opacity(0.5))
-            .blur(radius:2)
-            .offset(x: self.animateWaves ? -1*universalSize.width : 0)
-            .animation(
-                Animation.linear(duration: 20).repeatForever(autoreverses: false)
-            )
-            
-            // wave #2
-            Path {path in
-                path.move(
-                    to: CGPoint(
-                        x: 0,
-                        y: baseLineY + 20
+            // background
+            ZStack {
+                AngularGradient(
+                    gradient: Gradient(
+                        colors: [NirvanaColor.solidBlue, NirvanaColor.dimTeal, Color.orange, NirvanaColor.solidBlue]),
+                    center: .center,
+                    angle: .degrees(120))
+                 
+                LinearGradient(gradient: Gradient(
+                    colors: [NirvanaColor.white.opacity(0), NirvanaColor.white.opacity(1.0)]), startPoint: .bottom, endPoint: .top)
+                
+                getWave(peakPercentage: 0.4, troughPercentage: 0.65, peakAltercation: baseLineY + 150, troughAltercation: baseLineY - 90)
+                    .foregroundColor(NirvanaColor.dimTeal.opacity(0.5))
+                    .blur(radius:2)
+                    .offset(x: self.animateWaves ? -1*universalSize.width : 0)
+                    .animation(
+                        Animation.linear(duration: 20).repeatForever(autoreverses: false)
                     )
-                )
-
-                path.addCurve(
-                    to: CGPoint(x: universalSize.width, y: baseLineY + 30),
-                    control1: CGPoint(x: universalSize.width * 0.2, y: baseLineY - 50),
-                    control2: CGPoint(x: universalSize.width * 0.75, y: baseLineY + 100))
                 
-                path.addCurve(
-                    to: CGPoint(x: 2*universalSize.width, y: baseLineY),
-                    control1: CGPoint(x: universalSize.width * 1.2, y: baseLineY - 50),
-                    control2: CGPoint(x: universalSize.width * 1.75, y: baseLineY + 100))
-                
-                path.addLine(to: CGPoint(x: 2*universalSize.width, y: universalSize.height))
-                path.addLine(to: CGPoint(x: 0, y: universalSize.height))
-
-            }
-            .foregroundColor(Color.orange.opacity(0.3))
-            .blur(radius:2)
-            .offset(x: self.animateWaves ? -1*universalSize.width : 0)
-            .animation(
-                Animation.linear(duration: 10).repeatForever(autoreverses: false)
-            )
-            
-            // wave #3
-            Path {path in
-                path.move(
-                    to: CGPoint(
-                        x: 0,
-                        y: baseLineY - 20
+                getWave(peakPercentage: 0.2, troughPercentage: 0.75, peakAltercation: baseLineY - 50, troughAltercation: baseLineY + 100)
+                    .foregroundColor(Color.orange.opacity(0.3))
+                    .blur(radius:2)
+                    .offset(x: self.animateWaves ? -1*universalSize.width : 0)
+                    .animation(
+                        Animation.linear(duration: 10).repeatForever(autoreverses: false)
                     )
-                )
-
-                path.addCurve(
-                    to: CGPoint(x: universalSize.width, y: baseLineY),
-                    control1: CGPoint(x: universalSize.width * 0.25, y: baseLineY + 70),
-                    control2: CGPoint(x: universalSize.width * 0.75, y: baseLineY - 100))
                 
-                path.addCurve(
-                    to: CGPoint(x: 2*universalSize.width, y: baseLineY),
-                    control1: CGPoint(x: universalSize.width * 1.25, y: baseLineY + 70),
-                    control2: CGPoint(x: universalSize.width * 1.75, y: baseLineY - 100))
-                
-                
-                path.addLine(to: CGPoint(x: 2*universalSize.width, y: universalSize.height))
-                path.addLine(to: CGPoint(x: 0, y: universalSize.height))
+                getWave(peakPercentage: 0.25, troughPercentage: 0.75, peakAltercation: baseLineY + 70, troughAltercation: baseLineY - 100)
+                    .foregroundColor(NirvanaColor.teal.opacity(0.3))
+                    .blur(radius:2)
+                    .offset(x: self.animateWaves ? -1*universalSize.width : 0)
+                    .animation(
+                        Animation.linear(duration: 12).repeatForever(autoreverses: false)
+                    )
             }
-            .foregroundColor(NirvanaColor.teal.opacity(0.3))
-            .blur(radius:2)
-            .offset(x: self.animateWaves ? -1*universalSize.width : 0)
-            .animation(
-                Animation.linear(duration: 12).repeatForever(autoreverses: false)
-            )
-                
         }
         .onAppear() {
             self.animateWaves = true
         }
         .ignoresSafeArea(.all)
-        
+    }
+    
+    private func getWave(peakPercentage: Double, troughPercentage: Double, peakAltercation: CGFloat, troughAltercation: CGFloat) -> Path {
+        Path {path in
+            path.move(
+                to: CGPoint(
+                    x: 0,
+                    y: baseLineY
+                )
+            )
+
+            path.addCurve(
+                to: CGPoint(x: universalSize.width, y: baseLineY),
+                control1: CGPoint(x: universalSize.width * peakPercentage, y: peakAltercation),
+                control2: CGPoint(x: universalSize.width * troughPercentage, y: troughAltercation))
+            
+            path.addCurve(
+                to: CGPoint(x: 2*universalSize.width, y: baseLineY),
+                control1: CGPoint(x: universalSize.width * (1 + peakPercentage), y: peakAltercation),
+                control2: CGPoint(x: universalSize.width * (1 + troughPercentage), y: troughAltercation))
+            
+            
+            path.addLine(to: CGPoint(x: 2*universalSize.width, y: universalSize.height))
+            path.addLine(to: CGPoint(x: 0, y: universalSize.height))
+        }
     }
 }
 
