7933c46f38bf5672fa03ee358414dfc1f3785a2b
talksik <patel.arjun50@gmial.com>
2021-12-18 19:21:42 -0800

    decent spot with a lot of the add basic info page

---
+++ nirvana-ios/Globals/avatars.swift
@@ -0,0 +1,75 @@
+//
+//  avatars.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/18/21.
+//
+
+import Foundation
+
+struct Avatars {
+    static let avatarSystemNames = [
+        "Artboards_Diversity_Avatars_by_Netguru-1",
+        "Artboards_Diversity_Avatars_by_Netguru-2",
+        "Artboards_Diversity_Avatars_by_Netguru-3",
+        "Artboards_Diversity_Avatars_by_Netguru-4",
+        "Artboards_Diversity_Avatars_by_Netguru-5",
+        "Artboards_Diversity_Avatars_by_Netguru-6",
+        "Artboards_Diversity_Avatars_by_Netguru-7",
+        "Artboards_Diversity_Avatars_by_Netguru-8",
+        "Artboards_Diversity_Avatars_by_Netguru-9",
+        "Artboards_Diversity_Avatars_by_Netguru-10",
+        "Artboards_Diversity_Avatars_by_Netguru-11",
+        "Artboards_Diversity_Avatars_by_Netguru-12",
+        "Artboards_Diversity_Avatars_by_Netguru-13",
+        "Artboards_Diversity_Avatars_by_Netguru-14",
+        "Artboards_Diversity_Avatars_by_Netguru-15",
+        "Artboards_Diversity_Avatars_by_Netguru-16",
+        
+        "Artboards_Diversity_Avatars_by_Netguru-17",
+        "Artboards_Diversity_Avatars_by_Netguru-18",
+        "Artboards_Diversity_Avatars_by_Netguru-19",
+        "Artboards_Diversity_Avatars_by_Netguru-20",
+        "Artboards_Diversity_Avatars_by_Netguru-21",
+        "Artboards_Diversity_Avatars_by_Netguru-22",
+        "Artboards_Diversity_Avatars_by_Netguru-23",
+        
+        "Artboards_Diversity_Avatars_by_Netguru-24",
+        "Artboards_Diversity_Avatars_by_Netguru-25",
+        "Artboards_Diversity_Avatars_by_Netguru-26",
+        "Artboards_Diversity_Avatars_by_Netguru-27",
+        "Artboards_Diversity_Avatars_by_Netguru-28",
+        "Artboards_Diversity_Avatars_by_Netguru-29",
+        "Artboards_Diversity_Avatars_by_Netguru-30",
+        
+        "Artboards_Diversity_Avatars_by_Netguru-31",
+        "Artboards_Diversity_Avatars_by_Netguru-32",
+        "Artboards_Diversity_Avatars_by_Netguru-33",
+        "Artboards_Diversity_Avatars_by_Netguru-34",
+        "Artboards_Diversity_Avatars_by_Netguru-35",
+        "Artboards_Diversity_Avatars_by_Netguru-36",
+        "Artboards_Diversity_Avatars_by_Netguru-37",
+        
+        "Artboards_Diversity_Avatars_by_Netguru-38",
+        "Artboards_Diversity_Avatars_by_Netguru-39",
+        "Artboards_Diversity_Avatars_by_Netguru-40",
+        "Artboards_Diversity_Avatars_by_Netguru-41",
+        "Artboards_Diversity_Avatars_by_Netguru-42",
+        "Artboards_Diversity_Avatars_by_Netguru-43",
+        "Artboards_Diversity_Avatars_by_Netguru-44",
+        "Artboards_Diversity_Avatars_by_Netguru-45",
+        "Artboards_Diversity_Avatars_by_Netguru-46",
+        "Artboards_Diversity_Avatars_by_Netguru-47",
+        "Artboards_Diversity_Avatars_by_Netguru-48",
+        "Artboards_Diversity_Avatars_by_Netguru-49",
+        "Artboards_Diversity_Avatars_by_Netguru-50",
+        "Artboards_Diversity_Avatars_by_Netguru-52",
+        "Artboards_Diversity_Avatars_by_Netguru-53",
+        "Artboards_Diversity_Avatars_by_Netguru-54",
+        "Artboards_Diversity_Avatars_by_Netguru-55",
+        "Artboards_Diversity_Avatars_by_Netguru-56",
+        "Artboards_Diversity_Avatars_by_Netguru-57",
+        "Artboards_Diversity_Avatars_by_Netguru-58",
+        "Artboards_Diversity_Avatars_by_Netguru-59"
+    ]
+}
--- nirvana-ios/Views/AddBasicInfo/AddBasicInfoView.swift
+++ nirvana-ios/Views/AddBasicInfo/AddBasicInfoView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import NavigationStack
 
 struct AddBasicInfoView: View {
+    @EnvironmentObject var navigationStack : NavigationStack
+    
+    @State var firstName =  ""
+    @State var lastName = ""
+    
+    @State var selectedAvatarIndex: Int = 0
+    
     var body: some View {
         ZStack {
             // bg
@@ -18,7 +26,7 @@ struct AddBasicInfoView: View {
                 Color.clear
                 
                 Button {
-                    
+                    self.navigationStack.pop()
                 } label: {
                     Label("back", systemImage:"chevron.left")
                         .labelStyle(.iconOnly)
@@ -27,12 +35,76 @@ struct AddBasicInfoView: View {
                 .padding()
             }
             
-            // all content top down
+            // main content
             VStack {
+                // header logo area
                 LogoHeaderView()
                 
+                Text("Let's Build Your Profile")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(NirvanaColor.teal)
+                    .multilineTextAlignment(.center)
+                
+                // Horizontal scroll view to allow user to select the avatar he or she wants
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        ForEach(0..<Avatars.avatarSystemNames.count) { index in
+                            Image(Avatars.avatarSystemNames[index])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100, alignment: .center)
+                                .blur(radius: self.selectedAvatarIndex == index ? 0 : 4)
+                                .onTapGesture {
+                                    print("selected another avatar: \(index)")
+                                    
+                                    self.selectedAvatarIndex = index
+                                }
+                        }
+                    }
+                }
+                .padding()
+                                
+                // input first and last name
+                VStack {
+                    TextField("First Name", text: self.$firstName)
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .clipShape(Capsule())
+                        .keyboardType(.phonePad)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                    TextField("Last Name", text: self.$lastName)
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .clipShape(Capsule())
+                        .keyboardType(.phonePad)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
+                
+                
                 Spacer()
+                
+                // button to save information
+                Button {
+                    
+                } label: {
+                    Text("Save")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(NirvanaColor.teal)
+                        .clipShape(Capsule())
+                        .shadow(radius:10)
+                }
+                .padding()
             }
+            .frame(maxHeight: .infinity)
         }
     }
 }
