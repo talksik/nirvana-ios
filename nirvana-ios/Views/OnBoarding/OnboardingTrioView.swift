50f722bcf24355bcea5165984e3529924923b0f5
talksik <patel.arjun50@gmial.com>
2021-12-19 23:53:23 -0800

    simple adjustments, move on now

--- nirvana-ios/Views/OnBoarding/OnboardingTrioView.swift
+++ nirvana-ios/Views/OnBoarding/OnboardingTrioView.swift
@@ -14,20 +14,20 @@ struct OnboardingTrioView: View {
     var body: some View {
         ZStack(alignment: .topLeading) {
             TabView {
-                OnboardingTemplateView(imgName: "undraw_through_the_park_lxnl", mainLeadingActText: "Be", mainHighlightedActText: "yourself", mainTrailingActText: "again.", subActText: "No more endless feeds, media, fomo, anxiety...focus on you and be more present.")
+                OnboardingTemplateView(imgName: "undraw_through_the_park_lxnl", mainLeadingActText: "Be", mainHighlightedActText: "yourself", mainTrailingActText: "again.", subActText: "No more endless feeds, profiles, media, fomo, anxiety...focus on you and be more present.")
                 
-                OnboardingTemplateView(imgName: "undraw_connection_b-38-q", mainLeadingActText: "Be picky about your ", mainHighlightedActText: "inner circle.", mainTrailingActText: "", subActText: "You are who you hang out with. We kept things minimal because less is more...and we care.")
+                OnboardingTemplateView(imgName: "undraw_connection_b-38-q", mainLeadingActText: "Be picky about your", mainHighlightedActText: "inner circle.", mainTrailingActText: "", subActText: "You are who you hang out with. We kept things minimal because less is more...and we care.")
                 
-                OnboardingTemplateView(imgName: "undraw_voice_assistant_nrv7", mainLeadingActText: "Buttery smooth convos with your ", mainHighlightedActText: "authentic voice.", mainTrailingActText: "", subActText: "Less screen time, no more meaningless texts. Start mindfully conversing without all of the noise.", bottomActArea: AnyView(
+                OnboardingTemplateView(imgName: "undraw_voice_assistant_nrv7", mainLeadingActText: "Buttery smooth convos with your", mainHighlightedActText: "authentic voice.", mainTrailingActText: "", subActText: "Less screen time, no more meaningless texts. Start mindfully conversing without all of the noise.", bottomActArea: AnyView(
                         VStack {
                             Button {
                                 self.navigationStack.push(AddBasicInfoView())
                             } label: {
-                                Text("Start Your Detox")
+                                Text("Get Started")
                                     .bold()
                                     .foregroundColor(NirvanaColor.white)
                                     .frame(maxWidth: .infinity)
-                                    .padding(.vertical, 20)
+                                    .padding()
                                     .background(NirvanaColor.teal)
                                     .clipShape(Capsule())
                                     .shadow(radius:10)
