984cc9c3a3f6b80a226a70f3a127a7a335fc6f67
talksik <patel.arjun50@gmail.com>
2021-12-29 22:07:40 -0800

    starting basic demo convos ui

---
+++ nirvana-ios/Views/Convos/ConvosView.swift
@@ -0,0 +1,20 @@
+//
+//  ConvosView.swift
+//  nirvana-ios
+//
+//  Created by Arjun Patel on 12/29/21.
+//
+
+import SwiftUI
+
+struct ConvosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConvosView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConvosView()
+    }
+}
